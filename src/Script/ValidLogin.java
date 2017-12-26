0cd6c5ce689ce8488f2911d2883e5d2a6b54d67f
gurububly <34858975+gurububly@users.noreply.github.com>
2017-12-26 23:52:49 +0530

    hii

--- src/Script/ValidLogin.java
+++ src/Script/ValidLogin.java
@@ -1,5 +1,29 @@
 package Script;
 
-public class ValidLogin {
+import org.testng.annotations.Test;
 
+import generic.BaseTest;
+import generic.XL;
+import page.EnterTimeTrackPage;
+import page.LoginPage;
+
+public class ValidLogin extends BaseTest{
+@Test(priority=1,groups= {"smoke","login"})
+public void testValidLogin() {
+	String un=XL.getCellValue(SETTINGS_PATH, XL_PATH, 1, 0);
+	String pw=XL.getCellValue(SETTINGS_PATH, XL_PATH, 1, 1);
+	String eTitle=XL.getCellValue(SETTINGS_PATH, XL_PATH, 1, 2);
+	
+	//enter valid username
+	LoginPage l=new LoginPage(driver);
+	l.setUserName(un);
+	
+	//enter valid password
+	l.setPassword(pw);
+	//click login
+	l.clickLogin();
+	//check homepage is displayed
+	EnterTimeTrackPage e=new EnterTimeTrackPage(driver);
+	e.verifyTitle(driver, eTitle);
+}
 }
--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -1,8 +1,13 @@
 package generic;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
@@ -20,14 +25,23 @@ if(browser.equals("chrome")) {
 	driver=new ChromeDriver();
 }else {
 	driver=new FirefoxDriver();
-}	
-
 }
+String url=AL.getProperty(SETTINGS_PATH, "URL");
+driver.get(url);
 
-
-
-
-
-
-
+String strITO=AL.getProperty(SETTINGS_PATH, "ITO");
+long ITO=Long.parseLong(strITO);
+driver.manage().timeouts().implicitlyWait(ITO, TimeUnit.SECONDS);
+}
+@AfterMethod(alwaysRun=true)
+public void closeApp(ITestResult res) {
+	String name=res.getName();
+	int status=res.getStatus();
+	if(status==2) {
+		String imgpath=AL.getPhoto(driver, PHOTO_PATH, name);
+		Reporter.log("IMGpath:"+imgpath,true);
+	
+}
+driver.close();
+}
 }
