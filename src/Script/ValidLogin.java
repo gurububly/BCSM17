c83c7f6c8ca5e7341f757a403d597d388ea4b0dc
gurububly <34858975+gurububly@users.noreply.github.com>
2017-12-27 21:16:07 +0530

    hi

--- src/Script/ValidLogin.java
+++ src/Script/ValidLogin.java
@@ -10,7 +10,7 @@ import page.LoginPage;
 public class ValidLogin extends BaseTest{
 @Test(priority=1,groups= {"smoke","login"})
 public void testValidLogin() {
-	String un=XL.getCellValue(XL_PATH,"ValidLogin", 1, 0);
+String un=XL.getCellValue(XL_PATH,"ValidLogin", 1, 0);
 	String pw=XL.getCellValue(XL_PATH,"ValidLogin" ,1, 1);
 	String eTitle=XL.getCellValue(XL_PATH,"ValidLogin", 1, 2);
 	
